0e8d5ce9744fa593cf3030743587000f2428261d
caraz22 <cszoke22@gmail.com>
2025-06-26 20:18:36 -0500

    trying to print out festivals of season's vectors

--- escape-the-valley/main.cpp
+++ escape-the-valley/main.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include "Festivals.h"
+#include "Seasons.h"
+
 using namespace std;
+
+int main() {
+    Festivals festivalManager;
+
+    vector<Festival> springFestivals;
+    vector<Festival> summerFestivals;
+    vector<Festival> fallFestivals;
+    vector<Festival> winterFestivals;
+
+    festivalManager.addFestsToSeasons(springFestivals, spring);
+    festivalManager.addFestsToSeasons(summerFestivals, summer);
+    festivalManager.addFestsToSeasons(fallFestivals, fall);
+    festivalManager.addFestsToSeasons(winterFestivals, winter);
+
+    cout << "Hello, World!";
+}
